4e1e7da805911a7915d91a96d6a237c1c297c3b0
Jihun Kim <jihunkim101112@gmail.com>
2021-10-17 23:04:11 +0900

    class 폴더 업데이트

---
+++ class/pages/29-01-kakao-map/index.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import { useEffect } from "react";
+
+declare const window: typeof globalThis & {
+  kakao: any;
+};
+
+export default function KakaoMapPage() {
+  useEffect(() => {
+    const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+
+    const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    console.log(map);
+  });
+
+  return (
+    <>
+      <Head>
+        <script
+          type="text/javascript"
+          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=3d7187013f1e2a693255bd9b2639d9f7"
+        ></script>
+      </Head>
+      <div id="map" style={{ width: "500px", height: "400px" }}></div>
+    </>
+  );
+}
---
+++ class/pages/29-03-kakao-map-routed/index.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+
+declare const window: typeof globalThis & {
+  kakao: any;
+};
+
+export default function KakaoMapPage() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=3d7187013f1e2a693255bd9b2639d9f7";
+    document.head.appendChild(script);
+    script.onload = () => {
+      window.kakao.maps.load(function () {
+        const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+        const options = {
+          //지도를 생성할 때 필요한 기본 옵션
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+          level: 3, //지도의 레벨(확대, 축소 정도)
+        };
+
+        const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        console.log(map);
+
+        const marker = new window.kakao.maps.Marker({
+          // 지도 중심좌표에 마커를 생성합니다.
+          position: map.getCenter(),
+        });
+        // 지도에 마커를 표시합니다.
+        marker.setMap(map);
+
+        // 지도에 클릭 이벤트를 등록합니다
+        // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+        window.kakao.maps.event.addListener(
+          map,
+          "click",
+          function (mouseEvent: { latLng: any }) {
+            // 클릭한 위도, 경도 정보를 가져옵니다
+            var latlng = mouseEvent.latLng;
+
+            // 마커 위치를 클릭한 위치로 옮깁니다
+            marker.setPosition(latlng);
+          }
+        );
+      });
+    };
+  }, []);
+
+  return <div id="map" style={{ width: "500px", height: "400px" }}></div>;
+}
